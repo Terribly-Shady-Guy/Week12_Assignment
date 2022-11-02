f5294f41344b0c8aa74b7ba5e905fb3f007143e3
Terribly-Shady-Guy <tylerkaufmann98@gmail.com>
2022-11-01 21:06:33 -0500

    fixed code to output db connection status

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ mongoose.connect(connectionString, {
 const db = mongoose.connection;
 
 db.on("error", () => console.error.bind(console, "db connection error"));
-db.once("connect", () => console.log("connected to database successfully"));
+db.once("open", () => console.log("connected to database successfully"));
 
 //put code to get db schema here
 app.listen(PORT, () => console.log(`server started on http://localhost:${PORT}`));
